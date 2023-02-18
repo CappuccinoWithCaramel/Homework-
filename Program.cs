085a2d46f8ef82683a6adc4f7902b4c26afdd23c
Stanislav <neruhin2001@gmail.com>
2023-02-18 14:53:30 +0300

    asdasd

--- Program.cs
+++ Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1.
+
+// Console.WriteLine("Input number A");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number B");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// if (A>B)
+// {
+//     Console.WriteLine($"Max number is A - {A}");
+//     Console.WriteLine($"Min number is B - {B}");
+// }
+// else
+// {
+//     Console.WriteLine($"Max number is B - {B}");
+//     Console.WriteLine($"Min number is A - {A}");
+// }
+
+// Задача 2.
+
+// Console.WriteLine("Input numbers");
+// int num1= Convert.ToInt32(Console.ReadLine());
+// int num2= Convert.ToInt32(Console.ReadLine());
+// int num3= Convert.ToInt32(Console.ReadLine());
+// if (num1 > num2)
+// {
+//     if (num1 > num3)
+//     {
+//         Console.WriteLine($"Max number is num1 -{num1}");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Max number is num3 - {num3}");
+//     }
+// }
+// else
+// {
+//     if (num2 > num3)
+//     {
+//         Console.WriteLine($"Max number is num2 - {num2}");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Max number is num3 - {num3}");
+//     }
+// }
+
+// Задача 3.
+
+// Console.WriteLine("Input numbers");
+// int num= Convert.ToInt32(Console.ReadLine());
+
+// int remainder= num%2;
+
+// if (remainder == 0)
+// {
+//     Console.WriteLine($"Current number {num}  is even ");
+// }
+// else 
+// {
+//     Console.WriteLine($"Current number {num} is odd");
+// }
+
+// Задача 4.
+
+// Console.WriteLine("Input number");
+
+// int N= Convert.ToInt32(Console.ReadLine());
+
+// int current= 1;
+
+// while (current <= N)
+// {
+//     int remainder= current%2;
+//     if (remainder == 0)
+//     {
+//         Console.Write(current + ", " );
+//     }
+//     current ++;
+// }
