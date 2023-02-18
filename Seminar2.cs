65b7b377ed9cdf6b4e2e9d25ba12978ad726a78f
Stanislav <neruhin2001@gmail.com>
2023-02-18 14:44:20 +0300

    wwww

---
+++ Seminar2.cs
@@ -0,0 +1,81 @@
+
+
+// Homework 1.
+
+// int SecNum(int num)
+// {
+//     int dec= num / 10;
+//     int ost= dec % 10;
+//     return ost;
+// }
+
+// Console.WriteLine("Input number");
+
+// int num= Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"Second digit of {num} is {SecNum(num)}");
+
+// SecNum( num );
+
+
+
+// Homework 2.
+
+// int Digit(int num)
+// {
+//     if (num > 999)
+//     {
+//         while (num > 999)
+//         {
+//             num= num / 10;
+//         }
+//     int dig= num % 10;
+//     Console.WriteLine($"Third digit is {dig}");
+//     return dig;
+//     }
+//     else
+//     {
+//         Console.WriteLine("Number don't have a third digit");
+//         return num;
+//     }
+// }
+
+// Console.WriteLine("Input number");
+
+// int num=Convert.ToInt32(Console.ReadLine());
+
+// Digit(num);
+
+
+// Homework 3.
+
+// int Week(int num)
+// {
+//     if (num > 5)
+//     {
+//         if (num == 6)
+//         {
+//             Console.WriteLine("Yes, it's a day off");
+//         }
+//         if (num == 7)
+//         {
+//             Console.WriteLine("Yes, it's a day off");
+//         }
+//         if (num > 7)
+//         {
+//             Console.WriteLine("It's not a day of a week");
+//         }
+//     }
+//     else
+//     {
+//         Console.WriteLine("No, it's not a day off");
+//     }
+//     return num;
+// }
+
+// Console.WriteLine("Input number of day of a week");
+
+// int num=Convert.ToInt32(Console.ReadLine());
+
+// Week(num);
+
